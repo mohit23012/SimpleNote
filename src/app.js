6da11120a8ba2b1dee8ae690c5f17e3a57acd1cf
Mohit <mk5515003@gmail.com>
2023-12-25 14:50:03 +0530

    update overflowing

--- src/app.js
+++ src/app.js
@@ -15,21 +15,22 @@ Show();
 
 button.addEventListener("click", () => {
   let container = document.createElement("div");
-  container.className =
-    "relative my-4 p-5 h-20 bg-white rounded-md max-w-md focus:outline-none";
+  container.className = "relative my-4 p-5 h-20 bg-white rounded-md max-w-md focus:outline-none";
+  container.style.height = "100px"; // Set the desired height
+  container.style.overflowY = "auto"; // Add scroll effect for vertical overflow
 
   let line = document.createElement("p");
   line.className = "p-3 mb-4 focus:outline-none";
   line.setAttribute("contenteditable", "true");
 
   const img = document.createElement("img");
-  img.className = "absolute bottom-2 right-2 h-5 opacity-60 cursor-pointer";
+  img.className = "absolute bottom-2 r-2 h-5 opacity-60 cursor-pointer";
   img.setAttribute("src", "bin.jpg");
   img.setAttribute("alt", "Image");
 
-  sheet.appendChild(container);
   container.appendChild(line);
   container.appendChild(img);
+  sheet.appendChild(container);
 
   Storage();
 });
