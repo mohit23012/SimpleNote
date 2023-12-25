5901dd9e4375741b95d0e185a5e72b28ed71ddb9
Mohit <mk5515003@gmail.com>
2023-12-25 13:36:20 +0530

    updata

--- src/app.js
+++ src/app.js
@@ -13,33 +13,30 @@ const Show = () => {
 
 Show();
 
-//create note button
 button.addEventListener("click", () => {
-                   // Create a container div for better structure
   let container = document.createElement("div");
   container.className =
     "relative my-4 p-5 h-20 bg-white rounded-md max-w-md focus:outline-none";
 
-                   // Create a paragraph for text content
   let line = document.createElement("p");
   line.className = "p-3 mb-4 focus:outline-none";
   line.setAttribute("contenteditable", "true");
 
-                   // Create an image with alt text
   const img = document.createElement("img");
-  img.className = "absolute bottom-2 right-2 h-5 opacity-60 cursor-pointer ";
+  img.className = "absolute bottom-2 right-2 h-5 opacity-60 cursor-pointer";
   img.setAttribute("src", "bin.jpg");
   img.setAttribute("alt", "Image");
 
-                   // Append the img to the line to the container to the sheet
-  sheet.appendChild(container).appendChild(line).appendChild(img);
+  sheet.appendChild(container);
+  container.appendChild(line);
+  container.appendChild(img);
+
   Storage();
 });
 
-// delete button fn()
 sheet.addEventListener("click", (e) => {
   if (e.target.tagName === "IMG") {
-    e.target.parentElement.parentElement.remove();
+    e.target.parentElement.remove();
     Storage();
   } else if (e.target.tagName === "P") {
     Storage();
